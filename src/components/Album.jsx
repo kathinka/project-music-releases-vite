4ec89c0dcedee1ad4cc8fcd173df05f845f27953
Kathinka Sewell <4355167+kathinka@users.noreply.github.com>
2024-03-10 16:11:09 +0100

    formatting fix

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -8,48 +8,48 @@ import "./styling/album.css"
 //get the data *mArray* and "filter" it further down (albums)
 
 export const Album = ({ mArray }) => {
-	let all = mArray.albums.items;
-	//set default state
-	const [selected, setSelected] = useState(all);
+  let all = mArray.albums.items;
+  //set default state
+  const [selected, setSelected] = useState(all);
 
-	//Handle the different states - use filter to get new results
-	const handlePreview = (state) => {
-		if (state === "single") {
-			const single = all.filter(artist => artist.album_type === "single");
-			setSelected(single);
-			return single;
-		} else if (state === "album") {
-			const album = all.filter(artist => artist.album_type === "album");
-			setSelected(album);
-			return album;
-		} else if (state === "all") {
-			setSelected(all);
-			return all;
-		}
+  //Handle the different states - use filter to get new results
+  const handlePreview = (state) => {
+    if (state === "single") {
+      const single = all.filter(artist => artist.album_type === "single");
+      setSelected(single);
+      return single;
+    } else if (state === "album") {
+      const album = all.filter(artist => artist.album_type === "album");
+      setSelected(album);
+      return album;
+    } else if (state === "all") {
+      setSelected(all);
+      return all;
+    }
 
-	}
-	return (
-		//get user input from button - pass til handlePreview function with correct parameter
-		<div className="nav-wrapper">
-			<button id="single" className="type-btn" onClick={() => handlePreview("single")}>Singles</button>
-			<button id="album" className="type-btn" onClick={() => handlePreview("album")}>Albums</button>
-			<button id="all" className="type-btn" onClick={() => handlePreview("all")}>All</button>
+  }
+  return (
+    //get user input from button - pass til handlePreview function with correct parameter
+    <div className="nav-wrapper">
+      <button id="single" className="type-btn" onClick={() => handlePreview("single")}>Singles</button>
+      <button id="album" className="type-btn" onClick={() => handlePreview("album")}>Albums</button>
+      <button id="all" className="type-btn" onClick={() => handlePreview("all")}>All</button>
 
-			<section className="album-wrap">
-				{selected.map((newAlbum, index) => {
-					return (
-						<article className="album" key={index}>
-							<Cover coverInfo={newAlbum} />
-							<AlbumName albumInfo={newAlbum} />
-							<Artist artistInfo={newAlbum} />
-						</article>
+      <section className="album-wrap">
+        {selected.map((newAlbum, index) => {
+          return (
+            <article className="album" key={index}>
+              <Cover coverInfo={newAlbum} />
+              <AlbumName albumInfo={newAlbum} />
+              <Artist artistInfo={newAlbum} />
+            </article>
 
 
-					);
-				})
-				}
+          );
+        })
+        }
 
-			</section>
-		</div>
-	)
+      </section>
+    </div>
+  )
 };
